0fe5c2066b005a24c76dcc6b7ef7f39ec3855379
AldoVM11 <aldooscar123@gmail.com>
2023-08-03 23:26:36 -0600

    Continue

---
+++ src/data/Products.js
@@ -0,0 +1,75 @@
+const products = [ {
+        _id: "1",
+        name: "Laboris cillum nisi aliquip laboris",
+        image:
+            "https://i.pinimg.com/originals/c4/b9/0e/c4b90e35644d7e8b9000401857c197c6.png",
+        description:
+            "Qui esse laboris incididunt adipisicing ex dolor anim deserunt dolor qui adipisicing amet.  Commodo laborum anim commodo excepteur labore pariatur. Excepteur anim aute laborum incididunt quis officia aliquip. Duis ad ut cillum non in mollit duis aliqua ad laborum.",
+        price: 89,
+        countInStock: 3,
+        rating: 4,
+        numReviews: 4,
+    },
+    {
+        _id: "2",
+        name: "Laboris cillum nisi aliquip laboris",
+        image:
+            "https://chpic.su/_data/stickers/p/PeepoHD/PeepoHD_111.webp",
+        description:
+            "Qui esse laboris incididunt adipisicing ex dolor anim deserunt dolor qui adipisicing amet.  Commodo laborum anim commodo excepteur labore pariatur. Excepteur anim aute laborum incididunt quis officia aliquip. Duis ad ut cillum non in mollit duis aliqua ad laborum.",
+        price: 599,
+        countInStock: 10,
+        rating: 2,
+        numReviews: 3,
+    },
+    {
+        _id: "3",
+        name: "Laboris cillum nisi aliquip laboris",
+        image:
+            "https://pbs.twimg.com/media/FxfleHlX0AQ6iTo.png",
+        description:
+            "Qui esse laboris incididunt adipisicing ex dolor anim deserunt dolor qui adipisicing amet.  Commodo laborum anim commodo excepteur labore pariatur. Excepteur anim aute laborum incididunt quis officia aliquip. Duis ad ut cillum non in mollit duis aliqua ad laborum.",
+        price: 89,
+        countInStock: 3,
+        rating: 4,
+        numReviews: 4,
+    },
+    {
+        _id: "4",
+        name: "Laboris cillum nisi aliquip laboris",
+        image:
+            "https://storage.googleapis.com/sticker-prod/YGq5TTZZ2ijJUMDPHpEI/13.png",
+        description:
+            "Qui esse laboris incididunt adipisicing ex dolor anim deserunt dolor qui adipisicing amet.  Commodo laborum anim commodo excepteur labore pariatur. Excepteur anim aute laborum incididunt quis officia aliquip. Duis ad ut cillum non in mollit duis aliqua ad laborum.",
+        price: 89,
+        countInStock: 3,
+        rating: 4,
+        numReviews: 4,
+    },
+    {
+        _id: "5",
+        name: "Laboris cillum nisi aliquip laboris",
+        image:
+            "https://cdn.7tv.app/emote/63c1eb29e2d16742d3c990a9/4x.webp",
+        description:
+            "Qui esse laboris incididunt adipisicing ex dolor anim deserunt dolor qui adipisicing amet.  Commodo laborum anim commodo excepteur labore pariatur. Excepteur anim aute laborum incididunt quis officia aliquip. Duis ad ut cillum non in mollit duis aliqua ad laborum.",
+        price: 89,
+        countInStock: 3,
+        rating: 4,
+        numReviews: 4,
+    },
+    {
+        _id: "6",
+        name: "Laboris cillum nisi aliquip laboris",
+        image:
+            "https://cdn3.emoji.gg/emojis/5722_PeepoSnack.png",
+        description:
+            "Qui esse laboris incididunt adipisicing ex dolor anim deserunt dolor qui adipisicing amet.  Commodo laborum anim commodo excepteur labore pariatur. Excepteur anim aute laborum incididunt quis officia aliquip. Duis ad ut cillum non in mollit duis aliqua ad laborum.",
+        price: 89,
+        countInStock: 3,
+        rating: 4,
+        numReviews: 4,
+    },
+];
+
+export default products;
